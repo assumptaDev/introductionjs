310b91321517f7c3b8f1d59d2077bcdee8ff01bd
assumptaDev <assumptaoseluokoduwa@gmail.com>
2022-06-24 21:27:51 +0100

    Second Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let firstName = 'Assumpta.';
+let height = '6ft2.';
+let country = 'Nigeria.';
+let aboutMe = "My name is " + firstName + " I'm " + height + " I'm from " + country;
+console.log(aboutMe);
